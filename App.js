142065462dde4eab3184d11e5429707170e35e63
shubham <shubhammr21@gmail.com>
2021-03-12 11:31:58 +0530

    Created stack navigator

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import * as Font from 'expo-font'
+import { StatusBar } from "expo-status-bar"
+import React, { useState } from "react"
+import { StyleSheet, Text, View } from "react-native"
+import * as Font from "expo-font"
 import AppLoading from "expo-app-loading"
 
-
+import MealsNavigator from "./navigation/MealsNavigator"
 
 export default function App() {
   const [fontLoaded, setFontLoaded] = useState(false)
@@ -23,19 +23,14 @@ export default function App() {
       />
     )
   }
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <MealsNavigator />
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+})
